865dad724dbd6136e7b1c5f306025ecb822e8b44
Michael 'devbug' Williams <m.t.williams@live.com>
2013-03-31 14:20:28 -0700

    Fixed x86 get_execution_state servicer.

--- src/foundation/execution_state_x86.asm
+++ src/foundation/execution_state_x86.asm
@@ -1,21 +1,20 @@
 section .text
-global _foundation_execution_state_x86@0
-_foundation_execution_state_x86@0:
-  ; push ecx
-  ; mov ecx, [esp + 8]
-  ; mov dword [ecx], eax
-  ; mov eax, ecx
-  ; pop ecx
-  ; mov dword [eax + 4], ecx
-  ; mov dword [eax + 8], edx
-  ; mov dword [eax + 12], ebx
-  ; mov ecx, esp
-  ; sub ecx, 4
-  ; mov dword [eax + 16], ecx
-  ; mov dword [eax + 20], ebp
-  ; mov dword [eax + 24], esi
-  ; mov dword [eax + 28], edi
-  ; pop ecx
-  ; push ecx
-  ; mov dword [eax + 32], ecx
-  ret 4
+global _foundation_execution_state_x86
+_foundation_execution_state_x86:
+  push ecx
+  mov ecx, [esp + 16]
+  mov dword [ecx], eax
+  pop eax
+  mov dword [ecx + 4], eax ; ecx
+  mov dword [ecx + 8], edx
+  mov dword [ecx + 12], ebx
+  mov eax, esp
+  add eax, 16
+  mov dword [ecx + 16], eax ; esp
+  mov eax, [esp + 4]
+  mov dword [ecx + 20], eax ; ebp
+  mov dword [ecx + 24], esi
+  mov dword [ecx + 28], edi
+  mov dword eax, [esp + 8]
+  mov dword [ecx + 32], eax ; eip
+  ret
