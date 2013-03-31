1e553fbc8ab5b877d3be06129c541e32b9aa4f4a
Michael 'devbug' Williams <m.t.williams@live.com>
2013-03-31 01:53:18 -0700

    Defined MAKE_NEW and MAKE_DELETE helper macros.

--- include/foundation/allocator.h
+++ include/foundation/allocator.h
@@ -13,6 +13,8 @@
 #include <foundation/preprocessor.h>
 #include <foundation/assert.h>
 
+#include <new>
+
 namespace foundation {
   class FOUNDATION_EXPORT Allocator {
     FOUNDATION_NON_COPYABLE(Allocator);
@@ -39,5 +41,11 @@ namespace foundation {
       static Allocator& scratch();
   };
 } // foundation
+
+#define MAKE_NEW( _Type, _Allocator ) \
+  new ((_Allocator.alloc(sizeof(_Type), alignof(_Type)))) _Type
+
+#define MAKE_DELETE( _Type, _Allocator, _Instance ) \
+  do { _Instance->~_Type(); _Allocator.free((void*)_Instance); } while (0, 0)
 
 #endif // _FOUNDATION_ALLOCATOR_H_
