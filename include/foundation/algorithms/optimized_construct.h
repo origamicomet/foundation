131b3d9c711bc8ee335b35bc257bcc9c9848bced
Michael 'devbug' Williams <m.t.williams@live.com>
2013-06-15 14:27:52 -0700

    Added optimizied_construct.

---
+++ include/foundation/algorithms/optimized_construct.h
@@ -0,0 +1,34 @@
+// The author(s) disclaim copyright to this source-code.
+// Can be licensed under MIT (in case the public domain is not recognized).
+// More information can be found in the LICENSE file.
+
+#ifndef _FOUNDATION_ALGORITHMS_OPTIMIZED_CONSTRUCT_H_
+#define _FOUNDATION_ALGORITHMS_OPTIMIZED_CONSTRUCT_H_
+
+// Provides an optimized constructor that takes into account triviality.
+
+#include <foundation/detect.h>
+#include <foundation/compat.h>
+#include <foundation/config.h>
+
+namespace foundation {
+  template <typename T>
+  FOUNDATION_INLINE void optimized_construct(
+    T* objs, size_t len,
+    typename enable_if<is_trivially_destructable<T>::value,int>::type = 0 )
+  {
+    (void)objs;
+    (void)len;
+  }
+
+  template <typename T>
+  FOUNDATION_INLINE void optimized_construct(
+    T* objs, size_t len,
+    typename enable_if<!is_trivially_destructable<T>::value,int>::type = 0 )
+  {
+    for (size_t idx = 0; idx < len; ++idx)
+      new ((void*)&objs[idx]) T();
+  }
+} // foundation
+
+#endif // _FOUNDATION_ALGORITHMS_OPTIMIZED_CONSTRUCT_H_
