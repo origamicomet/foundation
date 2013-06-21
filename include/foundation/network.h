7639739e6e42ab474ff99828fbc91983ba72093e
Michael 'devbug' Williams <m.t.williams@live.com>
2013-06-21 14:48:46 -0700

    Implemented high-level Network::Packet helper.

---
+++ include/foundation/network.h
@@ -0,0 +1,12 @@
+// The author(s) disclaim copyright to this source-code.
+// Can be licensed under MIT (in case the public domain is not recognized).
+// More information can be found in the LICENSE file.
+
+#ifndef _FOUNDATION_NETWORK_H_
+#define _FOUNDATION_NETWORK_H_
+
+// Provides a high-leveling networking solutions (packets, protocols, etc.).
+
+#include <foundation/network/packet.h>
+
+#endif // _FOUNDATION_NETWORK_H_
